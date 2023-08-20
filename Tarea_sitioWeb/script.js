196b25dc3dbe655f6789348c442ccabb75cd729e
enriqueM02 <00016522@uca.edu.sv>
2023-08-20 00:53:08 -0600

    Por el momento, se termino el proyecto(creo)

--- Tarea_sitioWeb/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const saludoBtn = document.getElementById("saludoBtn");
-    const mensaje = document.getElementById("mensaje");
-
-    saludoBtn.addEventListener("click", function () {
-        mensaje.textContent = "¡Hola! Gracias por interactuar.";
-    });
-});
